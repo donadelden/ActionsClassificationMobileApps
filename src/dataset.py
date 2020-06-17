f0af8106f1ac01c4bfa65d426776c98f874109a4
Umberto Paro <umberto.paro@me.com>
2020-06-17 12:28:52 +0200

    DS: Grouped flows by action, separate test file

--- src/dataset.py
+++ src/dataset.py
@@ -1,51 +1,52 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import re
 
 data_dir = "data"
 data_file = "apps_total_plus_filtered"
 
 
-def dataset_mean_variance(data_dir=data_dir, data_file=data_file):
+def read_dataset(data_dir=data_dir, data_file=data_file):
     ds = pd.read_csv(f"{data_dir}/{data_file}.csv")
+    sequence = ds["sequence"].map(lambda l: int(l[9:]))
+    ds["sequence"] = sequence
+    # packets_length_total column is in string format, converting to numpy array
+    pkt_len = ds["packets_length_total"].map(
+        lambda l: tuple(np.fromstring(l[1:-1], sep=", ", dtype=np.float))
+    )
+    ds["packets_length_total"] = pkt_len
+    return ds
+
+
+def dataset_mean_variance(data_dir=data_dir, data_file=data_file):
+    ds = read_dataset(data_dir, data_file)
+    # group together flows related to the same instance of action
+    # and aggregate packet sequences
+    ds = (
+        ds[["app", "action", "sequence", "action_start", "packets_length_total"]]
+        .groupby(
+            ["app", "action", "sequence", "action_start"], sort=False, as_index=False
+        )
+        .agg(
+            {
+                "packets_length_total": lambda group: tuple(
+                    np.concatenate(group.tolist())
+                )
+            }
+        )
+    )
+
     app = ds["app"]
     action = ds["action"]
-    sequence = ds["sequence"].map(lambda l: int(re.sub(r"sequence_([0-9]+)", r"\1", l)))
+    sequence = ds["sequence"]
     mean = (
         ds["packets_length_total"]
-        .map(
-            lambda l: np.mean(np.abs(np.fromstring(l[1:-1], sep=", ", dtype=np.float)))
-        )
+        .map(lambda l: np.mean(np.abs(l)))
         .rename("packets_length_mean")
     )
     variance = (
         ds["packets_length_total"]
-        .map(lambda l: np.var(np.abs(np.fromstring(l[1:-1], sep=", ", dtype=np.float))))
-        .rename("packets_length_var")
+        .map(lambda l: np.std(np.abs(l)))
+        .rename("packets_length_std")
     )
     return pd.concat([app, action, sequence, mean, variance], axis=1)
-
-
-# ds = dataset_mean_variance()
-
-# import matplotlib.pyplot as plt
-
-# c = {
-#   'facebook': 'blue',
-#   'twitter': 'cyan',
-#   'gmail': 'red',
-#   'gplus': 'magenta',
-#   'tumblr': 'purple',
-#   'dropbox': 'yellow',
-#   'evernote': 'green'
-# }
-
-# plt.figure()
-# for u in ds['app'].unique():
-#   plt.scatter(ds['packets_length_mean'].where(ds['app'] == u), ds['packets_length_var'].where(ds['app'] == u), c=c[u], s=1., label=u)
-
-# plt.legend()
-# plt.xlabel('mean pkt lenght')
-# plt.ylabel('variance')
-# plt.show()
---
+++ test/test_dataset.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+import pandas as pd
+from dataset import dataset_mean_variance, read_dataset
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    ds = dataset_mean_variance()
+
+    c = {
+        "facebook": "blue",
+        "twitter": "cyan",
+        "gmail": "red",
+        "gplus": "magenta",
+        "tumblr": "purple",
+        "dropbox": "yellow",
+        "evernote": "green",
+    }
+
+    # ds = ds.query('app == "facebook" | app == "twitter"')
+    # ds = ds.query('sequence == 1')
+
+    plt.figure()
+    for u in ds["app"].unique():
+        plt.scatter(
+            ds["packets_length_mean"].where(ds["app"] == u),
+            ds["packets_length_std"].where(ds["app"] == u),
+            c=c[u],
+            s=1.0,
+            label=u,
+            alpha=0.3,
+        )
+
+    plt.legend()
+    plt.xlabel("mean pkt lenght")
+    plt.ylabel("variance")
+    plt.show()
