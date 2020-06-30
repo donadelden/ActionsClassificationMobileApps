0bea260213337af91d82f1b2067edff5c3d17867
Denis <denisdonadel@gmail.com>
2020-06-30 19:27:16 +0200

    CNN implementation

--- src/nn2.py
+++ src/nn2.py
@@ -10,24 +10,28 @@ class FFNN(tf.keras.Model):
     def __init__(self, output_dim):
         super(FFNN, self).__init__()
         self.weight_regularizer = tf.keras.regularizers.l2(1e-3)
-        self.hidden1 = tf.keras.layers.Dense(
-            10, activation=tf.nn.elu, kernel_regularizer=self.weight_regularizer
-        )
-        self.hidden2 = tf.keras.layers.Dense(
-            15, activation=tf.nn.sigmoid, kernel_regularizer=self.weight_regularizer
-        )
-        self.dropout1 = tf.keras.layers.Dropout(0.2)
-        self.dropout2 = tf.keras.layers.Dropout(0.1)
+
+        self.conv1 = tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation=tf.nn.relu)
         self.batchnorm1 = tf.keras.layers.BatchNormalization(momentum=0.999)
+        self.flatten1 = tf.keras.layers.Flatten()
+
+        self.conv2 = tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation=tf.nn.relu)
         self.batchnorm2 = tf.keras.layers.BatchNormalization(momentum=0.999)
+        self.flatten2 = tf.keras.layers.Flatten()
+
+        self.hidden2 = tf.keras.layers.Dense(15, activation=tf.nn.sigmoid, kernel_regularizer=self.weight_regularizer)
+        self.dropout2 = tf.keras.layers.Dropout(0.1)
+
         self.last = tf.keras.layers.Dense(output_dim, activation=tf.nn.softmax)
 
     def call(self, inputs, training=False):
-        x = self.hidden1(inputs)
+        x = self.conv1(inputs)
         x = self.batchnorm1(x, training)
-        x = self.dropout1(x, training)
-        x = self.hidden2(x)
+        x = self.flatten1(x)
+        x = self.conv2(inputs)
         x = self.batchnorm2(x, training)
+        x = self.flatten2(x)
+        x = self.hidden2(x)
         x = self.dropout2(x, training)
         return self.last(x)
 
@@ -36,25 +40,6 @@ if __name__ == "__main__":
     tf.keras.backend.set_floatx("float64")
     # load and split the dataset
     ds = dataset_windowed(K=200, stride=10)
-    ingress = (
-        ds["packets_length_total"]
-        .map(np.array)
-        .map(lambda l: l[l > 0])
-        .where(lambda s: s.map(lambda l: l.size) > 0)
-        .dropna()
-    )
-    ds["packets_length_mean_ingress"] = ingress.map(np.mean)
-    ds["packets_length_std_ingress"] = ingress.map(np.std)
-    egress = (
-        ds["packets_length_total"]
-        .map(np.array)
-        .map(lambda l: -l[l < 0])
-        .where(lambda s: s.map(lambda l: l.size) > 0)
-        .dropna()
-    )
-    ds["packets_length_mean_egress"] = egress.map(np.mean)
-    ds["packets_length_std_egress"] = egress.map(np.std)
-    ds = ds.drop(columns=["packets_length_total"]).dropna()
 
     X_train, X_test, y_train, y_test = train_test_split(
         ds.drop("app", axis=1),
@@ -72,8 +57,8 @@ if __name__ == "__main__":
     model = FFNN(len(ds["app"].unique()))
 
     ##### LEARNING PARAMETERS #####
-    epochs = 100
-    batch_size = 64
+    epochs = 15
+    batch_size = 32
     initial_lr = 1e-3
     lr_decay_rate = 0.97
 
@@ -95,8 +80,11 @@ if __name__ == "__main__":
         monitor="val_accuracy", min_delta=1e-4, patience=5, restore_best_weights=True,
     )
 
+    X_train_reshaped = pd.DataFrame(X_train["packets_length_total"].to_list())
+    X_train_reshaped = X_train_reshaped.to_numpy().reshape((-1, 200, 1))
+
     history = model.fit(
-        x=X_train,
+        x=X_train_reshaped,
         y=y_train_dumm,
         epochs=epochs,
         batch_size=batch_size,
@@ -109,12 +97,14 @@ if __name__ == "__main__":
     plots.train_val_history(history.history)
 
     # evaluation on test set
-    loss, acc = model.evaluate(X_test, y_test_dumm, batch_size=100)
+    X_test_reshaped = pd.DataFrame(X_test["packets_length_total"].to_list())
+    X_test_reshaped = X_test_reshaped.to_numpy().reshape((-1, 200, 1))
+    loss, acc = model.evaluate(X_test_reshaped, y_test_dumm, batch_size=100)
     print(f"Test loss: {loss}")
     print(f"Test accuracy: {acc}")
 
     y_pred_dumm = pd.DataFrame(
-        model.predict(X_test, batch_size=100), columns=y_test_dumm.columns,
+        model.predict(X_test_reshaped, batch_size=100), columns=y_test_dumm.columns,
     )
     y_pred = y_pred_dumm.idxmax(axis="columns").astype("category")
 
